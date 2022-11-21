4ef70c934092f5153c5a0161cca13a9efe1e9a71
facundopaez-dev <facundo.paez.dp@gmail.com>
2022-11-21 18:52:48 -0300

    Backend (model, stateless) de Permission, implementado

---
+++ ejemplos/CH3-EmployeeService/src/model/Permission.java
@@ -0,0 +1,46 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Permission {
+
+  @Id
+  @Column(name = "ID")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  public Permission() {}
+
+  public Permission(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String toString() {
+    return "Permission id: " + id + " name permission: " + name;
+  }
+  
+}

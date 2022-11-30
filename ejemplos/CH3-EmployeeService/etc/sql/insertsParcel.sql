5621d4294d95d3973a0d26988e69150ebf0d63d5
facundopaez-dev <facundo.paez.dp@gmail.com>
2022-11-29 22:57:41 -0300

    Archivo SQL insertsParcel, agregado

---
+++ ejemplos/CH3-EmployeeService/etc/sql/insertsParcel.sql
@@ -0,0 +1,47 @@
+-- Parcela con coordenadas geograficas de Puerto Madryn
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Puerto Madryn 1', 1, -42.7683337, -65.060855, '2018-01-15', 1);
+
+-- Parcela con coordenadas geograficas de Sierra Grande
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Sierra Grande 1', 4, -41.6098881, -65.3664475, '2016-02-22', 1);
+
+-- Parcela con coordenadas geograficas de CABA
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela CABA 1', 3, -34.6156625, -58.5033379, '2017-11-12', 1);
+
+-- Parcela con coordenadas geograficas de Viedma
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Viedma 1', 1, -40.8249902, -63.0176492, '2012-12-12', 1);
+
+-- Parcela con coordenadas geograficas de Cholila
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Cholila 1', 5, -42.5091569, -71.4351097, '2013-10-01', 1);
+
+-- Parcela con coordenadas geograficas de Esquel
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Esquel 1', 1.5, -42.9120944, -71.3396796, '2010-09-09', 1);
+
+-- Parcela con coordenadas geograficas de El Bolson
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela El Bolson 1', 3.5, -41.9657023, -71.5414559, '2009-05-25', 1);
+
+-- Parcela con coordenadas geograficas de Neuquen
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Neuquén 1', 5, -38.9411626, -68.1504201, '2008-03-03', 1);
+
+-- Parcela con coordenadas geograficas de Bahia Blanca
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Bahía Blanca 1', 1, -38.7210747, -62.3264118, '2007-12-01', 1);
+
+-- Parcela con coordenadas geograficas de San Rafael
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela San Rafael 1', 10, -34.6274175, -68.360229, '2006-05-01', 1);
+
+-- Parcela con coordenadas geograficas de Mina Clavero
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Mina Clavero 1', 8, -31.7290121, -65.0199968, '2011-03-29', 1);
+
+-- Parcela con coordenadas geograficas de Rosario
+INSERT INTO PARCEL (NAME, HECTARE, LATITUDE, LONGITUDE, REGISTRATION_DATE, ACTIVE) VALUES
+ ('Parcela Rosario 1', 5, -32.9520457, -60.766679, '2002-05-21', 1);

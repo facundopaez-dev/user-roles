35f028c097f7a37dbddce8d75c47badf64f25a53
facundopaez-dev <facundo.paez.dp@gmail.com>
2022-12-15 22:34:58 -0300

    Tarea #19 terminada

--- ejemplos/CH3-EmployeeService/src/servlet/LoginRestServlet.java
+++ ejemplos/CH3-EmployeeService/src/servlet/LoginRestServlet.java
@@ -79,4 +79,71 @@ public class LoginRestServlet {
     .build();
   }
 
+  @POST
+  @Path("/admin")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response authenticateAdmin(String json) throws IOException {
+    /*
+     * Para la autenticacion del administrador se deben hacer las siguientes comprobaciones:
+     * 1. La inexistencia del usuario ingresado. Si el usuario ingresado no existe (debido a que no esta registrado en el sistema), el
+     * servidor debe devolver como respuesta el mensaje HTTP 401 con el mensaje "Nombre de usuario o contraseña incorrectos".
+     * 
+     * 2. La autenticacion del usuario. Si el nombre de usuario y la contraseña no son iguales a los almacenados en la base de datos
+     * subyacente, el servidor debe devolver como respuesta el mensaje HTTP 401 (UNAUTHORIZED) con el mensaje "Nombre de usuario o contraseña incorrectos".
+     * La autenticacion es el proceso por el cual se confirma que algo (en este caso un usuario) es realmente quien dice ser.
+     * 
+     * 3. La posesion del permiso de super usuario (administrador). Si el usuario ingresado existe, es autentico y no tiene el permiso de
+     * super usuario, el servidor debe devolver como respuesta el mensaje HTTP 403 (FORBIDDEN) con el mensaje "Acceso no autorizado".
+     * 
+     * Las primeras dos comprobaciones se realizan al hacer la autenticacion del usuario.
+     */
+
+    User givenUser = mapper.readValue(json, User.class);
+
+    if (!userService.authenticate(givenUser.getUsername(), givenUser.getPassword())) {
+      return Response.status(Response.Status.UNAUTHORIZED)
+      .entity(new LoginResponse(LoginStatus.USERNAME_OR_PASSWORD_INCORRECT)).build();
+    }
+
+    /*
+     * La comprobacion del permiso de super usuario (administrador) del usuario nunca
+     * va a fallar en caso de que se ingrese un usuario inexistente, ya que primero
+     * se realiza la autenticacion, la cual cubre el caso de que el usuario ingresado
+     * no existe.
+     * 
+     * Por lo tanto, siempre y cuando se realice la autenticacion del usuario
+     * y luego la comprobacion del permiso de super usuario, dicha comprobacion
+     * nunca va a fallar en caso de que se ingrese un usuario inexistente.
+     */
+    if (!userService.checkSuperuserPermission(givenUser.getUsername())) {
+      return Response.status(Response.Status.FORBIDDEN).entity(new LoginResponse(LoginStatus.UNAUTHORIZED_ACCESS)).build();
+    }
+
+    JwtManager jwtManager = JwtManager.getInstance();
+
+    /*
+     * Recupera de la base de datos subyacente la unica clave
+     * secreta que hay para generar un JWT
+     */
+    SecretKey secretKey = secretKeyService.find();
+
+    /*
+     * Si el flujo de ejecucion de este metodo llega a este punto, es
+     * debido a que el usuario que inicia sesion es autentico y tiene
+     * el permiso de super usuario (administrador), por lo tanto, se
+     * lo recupera de la base de datos subyacente para usar su ID y su
+     * permiso en un JWT
+     */
+    givenUser = userService.findByUsername(givenUser.getUsername());
+
+    /*
+     * Si el usuario es autentico y tiene el permiso de super usuario (administrador),
+     * el servidor devuelve el mensaje HTTP 200 (OK) junto con un JWT que tiene el ID
+     * y el permiso del usuario, una fecha de emision y una fecha de expiracion
+     */
+    return Response.status(Response.Status.OK)
+    .entity(new Token(jwtManager.createJwt(givenUser.getId(), givenUser.getSuperuser(), secretKey.getValue())))
+    .build();
+  }
+
 }

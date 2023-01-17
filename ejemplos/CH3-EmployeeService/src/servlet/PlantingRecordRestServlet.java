a8fb8e42bfa33c92543091086643754189d1498c
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-01-16 21:55:09 -0300

    Tarea #68 terminada

--- ejemplos/CH3-EmployeeService/src/servlet/PlantingRecordRestServlet.java
+++ ejemplos/CH3-EmployeeService/src/servlet/PlantingRecordRestServlet.java
@@ -83,7 +83,7 @@ public class PlantingRecordRestServlet {
   @GET
   @Path("/{id}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Response find(@Context HttpHeaders request, @PathParam("id") int id) throws IOException {
+  public Response find(@Context HttpHeaders request, @PathParam("id") int plantingRecordId) throws IOException {
     Response givenResponse = RequestManager.validateAuthHeader(request, secretKeyService.find());
 
     /*
@@ -106,12 +106,36 @@ public class PlantingRecordRestServlet {
     }
 
     /*
+     * Obtiene el JWT del valor del encabezado de autorizacion
+     * de una peticion HTTP
+     */
+    String jwt = AuthHeaderManager.getJwt(AuthHeaderManager.getAuthHeaderValue(request));
+
+    /*
+     * Obtiene el ID de usuario contenido en la carga util del
+     * JWT del encabezado de autorizacion de una peticion HTTP
+     */
+    int userId = JwtManager.getUserId(jwt, secretKeyService.find().getValue());
+
+    /*
+     * Si al usuario que hizo esta peticion HTTP, no le pertenece
+     * el dato solicitado, la aplicacion del lado servidor
+     * devuelve el mensaje HTTP 403 (Forbidden) junto con el
+     * mensaje "Acceso no autorizado" (contenido en la clase
+     * PermissionResponse) y no se realiza la operacion
+     * solicitada
+     */
+    if (!service.checkUserOwnership(userId, plantingRecordId)) {
+      return Response.status(Response.Status.FORBIDDEN).entity(mapper.writeValueAsString(new PermissionResponse())).build();
+    }
+
+    /*
      * Si el valor del encabezado de autorizacion de la peticion HTTP
      * dada, tiene un JWT valido, la aplicacion del lado servidor
      * devuelve el mensaje HTTP 200 (Ok) junto con los datos solicitados
      * por el cliente
      */
-    return Response.status(Response.Status.OK).entity(mapper.writeValueAsString(service.find(id))).build();
+    return Response.status(Response.Status.OK).entity(mapper.writeValueAsString(service.find(userId, plantingRecordId))).build();
   }
 
   @POST
--- ejemplos/CH3-EmployeeService/src/stateless/PlantingRecordServiceBean.java
+++ ejemplos/CH3-EmployeeService/src/stateless/PlantingRecordServiceBean.java
@@ -1,15 +1,13 @@
 package stateless;
 
 import java.util.Collection;
-
 import javax.ejb.Stateless;
-
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-
-import model.PlantingRecord;
 import model.Parcel;
+import model.PlantingRecord;
 
 @Stateless
 public class PlantingRecordServiceBean {
@@ -100,6 +98,49 @@ public class PlantingRecordServiceBean {
    */
   public PlantingRecord find(int id) {
     return entityManager.find(PlantingRecord.class, id);
+  }
+
+  /**
+   * Retorna un registro de plantacion perteneciente a una de las
+   * parcelas de un usuario
+   * 
+   * @param userId
+   * @param plantingRecordId
+   * @return referencia a un objeto de tipo PlantingRecord perteneciente
+   * a una parcela del usuario con el ID dado
+   */
+  public PlantingRecord find(int userId, int plantingRecordId) {
+    Query query = getEntityManager().createQuery("SELECT r FROM PlantingRecord r JOIN r.parcel p WHERE (r.id = :plantingRecordId AND p.user.id = :userId)");
+    query.setParameter("plantingRecordId", plantingRecordId);
+    query.setParameter("userId", userId);
+
+    return (PlantingRecord) query.getSingleResult();
+  }
+
+  /**
+   * Comprueba si un registro de plantacion pertenece a un usuario
+   * dado, mediante la relacion muchos a uno que hay entre los
+   * modelos de datos PlantingRecord y Parcel.
+   * 
+   * Retorna true si y solo si el registro de plantacion pertenece
+   * al usuario dado.
+   * 
+   * @param userId
+   * @param plantingRecordId
+   * @return true si se encuentra el registro de plantacion con el
+   * ID y el ID de usuario provistos, false en caso contrario
+   */
+  public boolean checkUserOwnership(int userId, int plantingRecordId) {
+    boolean result = false;
+
+    try {
+      find(userId, plantingRecordId);
+      result = true;
+    } catch (NoResultException e) {
+      e.printStackTrace();
+    }
+
+    return result;
   }
 
 }

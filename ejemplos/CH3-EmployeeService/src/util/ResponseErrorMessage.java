c4a0ed78ba7a2f4ad0ab74dacd49b609c1481be4
facundopaez-dev <facundo.paez.sd@gmail.com>
2023-01-19 22:34:19 -0300

    Tarea #76 terminada

---
+++ ejemplos/CH3-EmployeeService/src/util/ResponseErrorMessage.java
@@ -0,0 +1,23 @@
+package util;
+
+/**
+ * ResponseErrorMessage es el enum que contiene los mensajes de error
+ * (como constantes de enumeracion) para las peticiones HTTP que por
+ * algun motivo no se pueden satisfacer
+ */
+public enum ResponseErrorMessage {
+  USERNAME_OR_PASSWORD_INCORRECT("Nombre de usuario o contraseña incorrectos"),
+  UNAUTHORIZED_ACCESS("Acceso no autorizado"),
+  RESOURCE_NOT_FOUND("Recurso no encontrado");
+
+  private final String message;
+
+  private ResponseErrorMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+}
